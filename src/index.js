1b18ce9fd9bf40824179a9ef24d25b9c8cde8e92
Sindhiya-96 <111561087+Sindhiya-96@users.noreply.github.com>
2022-12-08 02:00:37 -0700

    Update index.js
    test

--- src/index.js
+++ src/index.js
@@ -1,19 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter as Router, Route, Routes} from 'react-router-dom';
-
-import App from "./components/App";
-import Blog from "./components/Blog"
-import Contact from "./components/Contact";
-import CourseOne from "./components/CourseOne";
-import CourseTwo from "./components/CourseTwo";
-import CourseThree from "./components/CourseThree";
-import CourseFour from "./components/CourseFour";
-import Admin from "./components/Admin";
-import Awards from "./components/Awards";
-import Others from "./components/Others";
-import Interest from "./components/Interest";
-import InterestSub from "./components/InterestSub";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
