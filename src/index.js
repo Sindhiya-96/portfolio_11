29e50439d37de5927910a51b248639a68b987c5b
Sindhiya-96 <111561087+Sindhiya-96@users.noreply.github.com>
2022-12-08 02:19:32 -0700

    Update index.js
    test

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ import InterestSub from "./components/InterestSub";
 ReactDOM.render(
 <Router>
     <Routes>
-        <Route path="/portfolio_11" element={<App/>} />
+        <Route path="/" element={<App/>} />
         <Route path="/blog" element={<Blog/>} />
         <Route path="/contact" element={<Contact/>} />
         <Route path="/course_one" element={<CourseOne/>} />
