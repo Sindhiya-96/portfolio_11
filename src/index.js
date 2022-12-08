bf34e07861cf2004bf755528bfeeb482cebb9470
Sindhiya-96 <111561087+Sindhiya-96@users.noreply.github.com>
2022-12-08 02:14:19 -0700

    Update index.js
    test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter as Router, Route, Routes} from 'react-router-dom';
+import { HashRouter, Router, Route, Routes} from 'react-router-dom';
 
 import App from "./components/App";
 import Blog from "./components/Blog"
