9c18f2f2f0502b396f61a7e3403cb9491ea5c4da
Sindhiya-96 <111561087+Sindhiya-96@users.noreply.github.com>
2022-12-08 02:03:49 -0700

    Update index.js
    test

--- src/index.js
+++ src/index.js
@@ -1,9 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter as Router, Route, Routes} from 'react-router-dom';
+
+import App from "./components/App";
+import Blog from "./components/Blog"
+import Contact from "./components/Contact";
+import CourseOne from "./components/CourseOne";
+import CourseTwo from "./components/CourseTwo";
+import CourseThree from "./components/CourseThree";
+import CourseFour from "./components/CourseFour";
+import Admin from "./components/Admin";
+import Awards from "./components/Awards";
+import Others from "./components/Others";
+import Interest from "./components/Interest";
+import InterestSub from "./components/InterestSub";
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>Hello, worbjbjld!</h1>);
+
+
+ReactDOM.render(
+<Router>
+    <Routes>
+        <Route path="/portfolio_11" element={<App/>} />
+        <Route path="/blog" element={<Blog/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/course_one" element={<CourseOne/>} />
+        <Route path="/course_two" element={<CourseTwo/>} />
+        <Route path='/course_three' element={<CourseThree/>} />
+        <Route path='/course_four' element={<CourseFour/>} />
+        <Route path='/admin' element={<Admin/>}/>
+        <Route path='/awards' element={<Awards/>}/>
+        <Route path='/others' element={<Others/>}/>
+        <Route path='/interest' element={<Interest/>}/>
+        <Route path='/interestsub' element={<InterestSub/>}/>
+    </Routes>
+</Router>
+
+    ,document.getElementById("root"));
+
 
 
 
